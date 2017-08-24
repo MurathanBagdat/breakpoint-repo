fd4ed58d5bf13200bd4a9a25ef78eeb073114213
Murathan Bagdat <murathanbagdat@hotmail.com>
2017-08-24 20:36:56 +0300

    meVC is complete!

---
+++ BreakPoint/meVC.swift
@@ -0,0 +1,28 @@
+//
+//  meVC.swift
+//  BreakPoint
+//
+//  Created by Melisa Kısacık on 24.08.2017.
+//  Copyright © 2017 MurathanBagdat. All rights reserved.
+//
+
+import UIKit
+
+class meVC: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userEmailLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    //Actions###
+    @IBAction func signoutButtonPrsd(_ sender: UIButton) {
+    }
+ 
+}
